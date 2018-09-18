35b2ab47febbc72a7cd7ba1ca5f1615242b6ce00
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-09-18 00:27:18 -0400

    add auth.rb file in libs directory

---
+++ lib/auth.rb
@@ -0,0 +1,15 @@
+require 'jwt'
+require 'pry'
+class Auth
+
+  def self.create_token(user_object)
+    payload = {user: JSON.parse(user_object.to_json)}
+    token = JWT.encode(payload, "school project", "HS256")
+  end
+
+  def self.decode_token(token)
+    decode = JWT.decode(token, "school project", true, {algorithm: "HS256"})
+    return decode[0]["user"]
+  end
+
+end
