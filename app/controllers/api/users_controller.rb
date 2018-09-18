c22b060b3e489a00b61b653a1eb909b519a94286
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-09-18 01:03:44 -0400

    Add jwt gem to gemfile. add routes to routes.rb file

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,5 @@
+require 'auth'
+
 class Api::UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :update, :destroy]
@@ -8,12 +10,13 @@ class Api::UsersController < ApplicationController
   end
 
   def create
+    
     user = User.new(user_params)
     if user.save
       
       token = Auth.create_token(user)
       returned_user = Auth.decode_token(token)
-      render json: {user: { id: user.id, username: user.username, email: user.email, no_of_checkins: user.no_of_checkins, user_locations_attributes: user.user_locations }, token: token}, status: 200
+      render json: {user: { id: user.id, username: user.username, email: user.email, token: token}}, status: 200
       
     else
       render json: {message: user.errors}, status: 400
@@ -38,7 +41,7 @@ class Api::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:password, :password_confirmation, :username, :email
+    params.require(:user).permit(:password, :password_confirmation, :username, :email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #devise_for :users
+  
+  namespace :api do
+    resources :users, only: [:index, :show, :edit, :update, :destroy, :create]
+    resources :projects
+    resources :comments
+    resources :sessions, only: [:create, :destroy]
+  end
 end
