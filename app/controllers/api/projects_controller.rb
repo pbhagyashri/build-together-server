7d3b403565b05c01a5902a2435de8f0e2c90fd85
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-10-08 12:24:05 -0400

    Remove sorting function from index action on UsersController. Remove returned_user from sessionsController

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -12,18 +12,14 @@ class Api::ProjectsController < ApplicationController
   end
 
   def create 
-    
+
     project = Project.new(project_params)
-    binding.pry
-    if project.save
       
-      render json: project, status: 200
-
-    else
-
-      render json: {message: project.errors}, status: 400
-    
-    end
+      if project.save
+        render json: {name: project.name, technology: project.technology, description: project.description, duration: project.duration, num_of_likes: project.likes}, status: 200
+      else
+        render json: {message: project.errors}, status: 400
+      end
 
   end
 
@@ -39,7 +35,7 @@ class Api::ProjectsController < ApplicationController
   private 
 
   def project_params
-    params.require(:project).permit(:name, :technologies, :description, :duration, :user_id, :user_name, :github_link, :comments => [])
+    params.require(:project).permit(:name, :technologies, :description, :duration, :github_link, :comments => [])
   end
 
   def set_project
--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,16 +3,14 @@ require 'auth'
 class Api::SessionsController < ApplicationController
 
   def create
-    
+  
     user = User.find_by(email: params[:user][:email])
     
     if user && user.valid_password?(params[:user][:password])
       token = Auth.create_token(user)
-      returned_user = Auth.decode_token(token)
-      binding.pry
       render json: {user: { id: user.id, username: user.username, email: user.email }, token: token}, status: 200
     else
-      render json: {errors: "Email or Password is incorrect"}, status: 500
+      render json: {errors: "Email or Password is incorrect"}, status: 400
     end
 
   end
