597841d846a5d764193d33cbeb8fa0c81dca5521
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-10-10 14:47:26 -0400

    in Create action of ProjectsController, extract user's token from request header. add authenticate_user(token) helper method that takes in the token and authenticate user. build new project instance with project_params.

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,3 +1,5 @@
+require 'auth'
+
 class Api::ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy, :project_comments]
 
@@ -13,27 +15,45 @@ class Api::ProjectsController < ApplicationController
   end
 
   def create 
-
-    project = Project.new(project_params)
-      
+  
+    token = request.headers["HTTP_AUTHORIZATION"]
+    
+    current_user = authenticate_user(token) 
+    if current_user
+      project = current_user.projects.build(project_params)
       if project.save
         render json: {name: project.name, technology: project.technology, description: project.description, duration: project.duration, num_of_likes: project.likes}, status: 200
       else
         render json: {message: project.errors}, status: 400
       end
-
+    end
   end
 
   def show
+    if @project
+      render json: @project, status: 200
+    else
+      render json: {message: "Project not found"}, status: 400
+    end
   end
 
   def update
+    if @project.update(project_params)
+      render json: @project, status: 200
+    else
+      render json: {error: @project.errors}, status: 400
+    end
   end
 
   def destroy
   end
 
   private 
+
+  def authenticate_user(token)
+    user_id = Auth.decode_token(token)["id"]
+    current_user = User.find_by(id: user_id)
+  end
 
   def project_params
     params.require(:project).permit(:name, :technologies, :description, :duration, :github_link, :comments => [])
